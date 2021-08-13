a2c111b8311b2446cd562843dd2d77735fcdcb75
Rahul prabhu <rahul.prabu.07@gmail.com>
2021-08-13 20:55:27 +0530

    feat:Welcome to Stack and Queue program using LinkedList

---
+++ StackandQueue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackandQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Stack and Queue Programs");
+        }
+    }
+}
