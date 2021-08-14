6b7ce7c79067ec238666c72e8b8953ebacb8dc42
Rahul prabhu <rahul.prabu.07@gmail.com>
2021-08-14 08:30:36 +0530

    feat:Push elements in Stack using LinkedList

--- StackandQueue/Program.cs
+++ StackandQueue/Program.cs
@@ -7,6 +7,11 @@ namespace StackandQueue
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to Stack and Queue Programs");
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            linkedListStack.Display();
         }
     }
 }
